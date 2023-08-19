bcc7bd6972d1a7552a3131c9bb64646762849947
hyena <goatsfish@gmail.com>
2023-08-19 20:15:15 +0900

    init: project

---
+++ src/main/java/hyena/oauthlogin/OauthLoginApplication.java
@@ -0,0 +1,13 @@
+package hyena.oauthlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hyena/oauthlogin/OauthLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package hyena.oauthlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
